0618b6ac789664b566664f80da9a25e6d777096d
blameleo <leonardgre@gmail.com>
2023-03-24 17:21:27 +0000

    added log in functionality and authentication.

--- ecommerce-app/src/context/userContext.js
+++ ecommerce-app/src/context/userContext.js
@@ -1,62 +1,96 @@
-import { createContext  } from "react";
+import { createContext, useState, useEffect } from "react";
 import { useFormik } from "formik";
-import * as Yup from "yup"
+import * as Yup from "yup";
 
-export const    UserContext = createContext("")
+export const UserContext = createContext("");
 
 export function UserContextProvider(props) {
+  const [registered, setRegistered] = useState(false);
+const [ loggedIn, setLoggedIn] = useState(false)
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
 
-    const formik = useFormik({
-        initialValues: {
-            email: "",
-            password: "",
-            confirmPassword: ""
-        },
-
-        validationSchema: Yup.object({
-            email: Yup.string().email("email is invalid").required(),
-            password: Yup.string().required('Password is required'),
-    confirmPassword: Yup.string()
-     .oneOf([Yup.ref('password'), null], 'Passwords must match')
-        }),
-        onSubmit: (values)=>{
-            console.log(values );
-        }
-    } )
+    validationSchema: Yup.object({
+      email: Yup.string().email("email is invalid").required(),
+      password: Yup.string().required("Password is required"),
+      confirmPassword: Yup.string().oneOf(
+        [Yup.ref("password"), null],
+        "Passwords must match"
+      ),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      window.localStorage.setItem("values", JSON.stringify(values));
+      resetForm({ values: "" });
+      setRegistered(true);
+    },
+  });
 
 
-       
-    //   function getUserInfo(event) {
-    //     // let emailName = event.target.name
-    //     // let password = event.target.name
-    //     // let confirmPassword = event.target.name
+  const formikLogin = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
 
-    //     setUserData((prev)=>{
-    //         return {
-    //             ...prev,
-    //             [event.target.name]: event.target.value,
-    //             [event.target.name]: event.target.value,
-    //             [event.target.name]: event.target.value
-    //         }
-    //     })
-    //   }
+    validationSchema: Yup.object({
+      email: Yup.string().email("email is invalid").required(),
+      password: Yup.string().required("Password is required")
 
-    //   function clearForm(){
-    //     setUserData(
-    //         {
-    //             email: "",
-    //             password: "",
-    //             confirmPassword: ""
-    //           }
-    //     )
-    //   }
+    }),
+    onSubmit: (values, { resetForm }) => {
+        
+            if (
+              values.email === user.email &&
+              values.password === user.password
+            ) {
+              console.log(" logged in");
+                setLoggedIn(true)
+              resetForm({ values: "" });
 
-      
- 
+            }
+          
+    },
+  });
 
-    return (
-        <UserContext.Provider value={{ formik} } >
-            {props.children}
-        </UserContext.Provider>
-    )
+  let user = JSON.parse(localStorage.getItem("values"));
+  
+
+
+
+  useEffect(() => {});
+
+  //   function getUserInfo(event) {
+  //     // let emailName = event.target.name
+  //     // let password = event.target.name
+  //     // let confirmPassword = event.target.name
+
+  //     setUserData((prev)=>{
+  //         return {
+  //             ...prev,
+  //             [event.target.name]: event.target.value,
+  //             [event.target.name]: event.target.value,
+  //             [event.target.name]: event.target.value
+  //         }
+  //     })
+  //   }
+
+  //   function clearForm(){
+  //     setUserData(
+  //         {
+  //             email: "",
+  //             password: "",
+  //             confirmPassword: ""
+  //           }
+  //     )
+  //   }
+
+  return (
+    <UserContext.Provider value={{ formik, formikLogin , registered , loggedIn}}>
+      {props.children}
+    </UserContext.Provider>
+  );
 }
--- ecommerce-app/src/pages/Login.jsx
+++ ecommerce-app/src/pages/Login.jsx
@@ -1,32 +1,69 @@
-import React , {useState}from "react";
+import React, { useState, useContext } from "react";
 // import Button from "../components/Button";
 import SecNavbar from "../components/SecNavbar";
 import { Link } from "react-router-dom";
+import { UserContext } from "../context/userContext";
+import Home from "./Home";
 
 export default function Login() {
   const [loginPage, setLoginPage] = useState(true);
+  const { formikLogin, loggedIn } = useContext(UserContext);
 
+  console.log(loggedIn);
   return (
-    <div className="">
-      <SecNavbar text="create an account"  data={loginPage}/ >
-     
+    <div>
+{ loggedIn ? <Home/> :   <div className="">
+      <SecNavbar data={loginPage} datafunc={setLoginPage} />
+
       <h1 className="text-center text-2xl font-bold pt-32">Sign in</h1>
       <div className="flex justify-center items-center h-64">
-        <form action="" className="flex flex-col">
+        <form
+          action=""
+          className="flex flex-col"
+          onSubmit={formikLogin.handleSubmit}
+        >
           <div className="flex flex-col mb-5">
-            <label className="text-sm  text-left" htmlFor="">Email Address</label>
-            <input type="text" className=" w-72 bg-gray-100 h-12 p-3 outline-none mt-2" />
+            <label className="text-sm  text-left" htmlFor="">
+              Email Address
+            </label>
+            <input
+              type="text"
+              className=" w-72 bg-gray-100 h-12 p-3 outline-none mt-2"
+              value={formikLogin.values.email}
+              onChange={formikLogin.handleChange}
+              name="email"
+            />
+            {formikLogin.errors.email ? (
+              <p className="text-red-500">{formikLogin.errors.email}</p>
+            ) : null}
           </div>
 
           <div className="flex flex-col">
-          <label htmlFor="" className="text-sm text-left">password</label>
-          <input type="password" className=" w-72 bg-gray-100 h-12 p-3 outline-none mt-2" name="password" />
-
+            <label htmlFor="" className="text-sm text-left">
+              password
+            </label>
+            <input
+              type="password"
+              className=" w-72 bg-gray-100 h-12 p-3 outline-none mt-2"
+              name="password"
+              value={formikLogin.values.password}
+              onChange={formikLogin.handleChange}
+            />
+            {formikLogin.errors.password ? (
+              <p className="text-red-500">{formikLogin.errors.password}</p>
+            ) : null}
           </div>
 
-          <Link onClick={()=>{ setLoginPage(false)}} href="" className="bg-blue-900 text-white text-center mt-5 py-2 rounded">login</Link>
+          <button
+            type="submit"
+            className="bg-blue-900 text-white text-center mt-5 py-2 rounded"
+          >
+            login
+          </button>
         </form>
       </div>
+    </div>}
     </div>
+
   );
 }
